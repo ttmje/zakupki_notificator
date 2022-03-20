fbad76ea37a79bf3d9947f30128242cf066973c8
ttmje <78736591+ttmje@users.noreply.github.com>
2022-03-20 21:38:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=["start"])
 async def start_bot(message: types.Message):
     await bot.send_message(message.chat.id,
-                           f"Привет! !!!228TEST228!!!  \nЭтот бот автоматически забирает информацию по электронным закупкам с сайта zakupki.gov.ru"
+                           f"Привет! !!!DEPLOY TEST!!!  \nЭтот бот автоматически забирает информацию по электронным закупкам с сайта zakupki.gov.ru"
                            f"\nСейчас в базе уже {db.show_last_id()} закупок!")
     await message.reply('Ага, я уже работаю над получением закупок!')
 
